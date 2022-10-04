d79345da11a8798c38f2d801db70ce8423e98307
YuboMao <mao00071@morris.umn.edu>
2022-10-04 15:23:53 -0500

    fix fix a little

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,23 +1,6 @@
 #include "mergesort.h"
 #define MAX_SIZE 10
 
-void mergesort(int size, int values[]) {
-    int *l = a;
-    int *r = a + size/2;
-    int l_size = size/2;
-    int r_size = size - r_size;
-    if(size > 1){
-        mergesort(l_size, l);
-        mergesort(r_size, r);
-        merging(l, l_size, r, r_size);
-    }
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
-}
-
 void merging(int *l, int l_size, int *r, int r_size){
 	int i, j, k, temp[MAX_SIZE];
 	i=j=k=0;
@@ -34,7 +17,26 @@ void merging(int *l, int l_size, int *r, int r_size){
     while(j<r_size){
         temp[k++] = r[j++];
     }
-    for(int i=0, i< l_size + r_size, i++){
+    for(int i=0; i<l_size + r_size; i++){
         l[i] = temp[i];
     }
 }
+
+void mergesort(int size, int values[]) {
+    int *l = values;
+    int *r = values + size/2;
+    int l_size = size/2;
+    int r_size = size - r_size;
+    if(size > 1){
+        mergesort(l_size, l);
+        mergesort(r_size, r);
+        merging(l, l_size, r, r_size);
+    }
+  // Remember that a key goal here is to learn to use
+  // `malloc/calloc` and `free`, so make sure you explicitly
+  // allocate any new arrays that you need, even if you
+  // might not strictly need to.
+  return;
+}
+
+
