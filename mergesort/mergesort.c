c3e2e23562b57bf00194036552e3664fad16a485
YuboMao <mao00071@morris.umn.edu>
2022-10-04 15:13:03 -0500

    write mergesort method

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,40 @@
 #include "mergesort.h"
+#define MAX_SIZE 10
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
+    int *l = a;
+    int *r = a + size/2;
+    int l_size = size/2;
+    int r_size = size - r_size;
+    if(size > 1){
+        mergesort(l_size, l);
+        mergesort(r_size, r);
+        merging(l, l_size, r, r_size);
+    }
   // Remember that a key goal here is to learn to use
   // `malloc/calloc` and `free`, so make sure you explicitly
   // allocate any new arrays that you need, even if you
   // might not strictly need to.
   return;
+}
+
+void merging(int *l, int l_size, int *r, int r_size){
+	int i, j, k, temp[MAX_SIZE];
+	i=j=k=0;
+	while (i<l_size && j<r_size){
+		if(l[i]<r[j]){
+            temp[k++] = l[i++];
+		} else {
+            temp[k++] = r[j++];
+        }
+	}
+    while(i<l_size){
+        temp[k++] = l[i++];
+    }
+    while(j<r_size){
+        temp[k++] = r[j++];
+    }
+    for(int i=0, i< l_size + r_size, i++){
+        l[i] = temp[i];
+    }
 }
